8ff31ffbe31cb6e6c55d5e83179c5279d06cddb5
Flashky <mevengar@gmail.com>
2021-12-02 10:37:15 +0100

    Refactor to use template method pattern

--- src/main/java/com/adventofcode/flashk/day2/SubmarineMoveAim.java
+++ src/main/java/com/adventofcode/flashk/day2/SubmarineMoveAim.java
@@ -1,50 +1,20 @@
 package com.adventofcode.flashk.day2;
 
-import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+public class SubmarineMoveAim extends AbstractSubmarineMove {
 
-public class SubmarineMoveAim {
-
-	private Integer depth = 0;
-	private Integer horizontalPosition = 0;
 	private Integer aim = 0;
-	
-	private final static String COMMAND_PATTERN = "(forward|down|up) ([0-9]*)";
-	private Pattern pattern = Pattern.compile(COMMAND_PATTERN);
-	
-	public Integer solve(List<String> movements) {
-		
-		for(String movement : movements) {
-			
-			Matcher matcher = pattern.matcher(movement);
-			matcher.find();
 
-			String action = matcher.group(1);
-			Integer value = Integer.valueOf(matcher.group(2));
-			
-			switch(action) {
-				case "forward": {
-					horizontalPosition += value;
-					depth += aim * value;
-				} break;
-				case "down": aim += value; break;
-				case "up": aim	 -= value; break;
-				default: break;
-			}
-			
-		}
-		
-		return depth * horizontalPosition;
+	protected void forward(Integer value) {
+		this.horizontalPosition += value;
+		this.depth += aim * value;
 	}
 
-	public Integer getDepth() {
-		return depth;
+	protected void up(Integer value) {
+		this.aim -= value;
 	}
 
-
-	public Integer getHorizontalPosition() {
-		return horizontalPosition;
+	protected void down(Integer value) {
+		 this.aim += value;
 	}
 	
 	public Integer getAim() {
