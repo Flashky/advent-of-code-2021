c608660a4603df84673532ee8a664bac30706518
Flashky <mevengar@gmail.com>
2021-12-02 09:44:34 +0100

    Refactor to use regex patterns

--- src/main/java/com/adventofcode/flashk/day2/SubmarineMoveAim.java
+++ src/main/java/com/adventofcode/flashk/day2/SubmarineMoveAim.java
@@ -1,6 +1,8 @@
 package com.adventofcode.flashk.day2;
 
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SubmarineMoveAim {
 
@@ -8,13 +10,18 @@ public class SubmarineMoveAim {
 	private Integer horizontalPosition = 0;
 	private Integer aim = 0;
 	
+	private final static String COMMAND_PATTERN = "(forward|down|up) ([0-9]*)";
+	private Pattern pattern = Pattern.compile(COMMAND_PATTERN);
+	
 	public Integer solve(List<String> movements) {
 		
 		for(String movement : movements) {
 			
-			String[] commands = movement.split(" ");
-			String action = commands[0];
-			Integer value = Integer.valueOf(commands[1]);
+			Matcher matcher = pattern.matcher(movement);
+			matcher.find();
+
+			String action = matcher.group(1);
+			Integer value = Integer.valueOf(matcher.group(2));
 			
 			switch(action) {
 				case "forward": {
