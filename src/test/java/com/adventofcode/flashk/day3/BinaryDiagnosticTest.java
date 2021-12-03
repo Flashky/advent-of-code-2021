63ee20aa190494f91b3f511023fc7e8c0ad6a81f
Flashky <mevengar@gmail.com>
2021-12-03 11:12:03 +0100

    Solve day 3 puzzle

---
+++ src/test/java/com/adventofcode/flashk/day3/BinaryDiagnosticTest.java
@@ -0,0 +1,98 @@
+package com.adventofcode.flashk.day3;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.junit.Test;
+
+public class BinaryDiagnosticTest {
+
+	private final static String INPUT_FOLDER = "day3/";
+	private final static String INPUT_FILE = "data.input";
+	private final static String INPUT_FILE_SAMPLE = "sample.input";
+	
+	@Test
+	public void testSolvePowerConsumption() throws URISyntaxException, IOException {
+		
+		// Read input file
+		List<String> input = readFile(INPUT_FILE);
+		
+		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic();
+		
+		int result = binaryDiagnostic.solvePowerConsumption(input);
+		//System.out.println(result);
+		assertEquals(2967914, result);
+	}
+	
+	@Test
+	public void testSolvePowerConsumptionSample() throws URISyntaxException, IOException {
+		
+		// Read input file
+		List<String> input = readFile(INPUT_FILE_SAMPLE);
+		
+		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic();
+		
+		int result = binaryDiagnostic.solvePowerConsumption(input);
+		
+		assertEquals(198, result);
+	}
+	
+	@Test
+	public void testSolveLifeSupportRatingSample() throws URISyntaxException, IOException {
+		
+		// Read input file
+		List<String> input = readFile(INPUT_FILE_SAMPLE);
+		
+		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic();
+		
+		int result = binaryDiagnostic.solveLifeSupportRating(input);
+		assertEquals(230, result);
+	}
+	
+	@Test
+	public void testSolveLifeSupportRating() throws URISyntaxException, IOException {
+		
+		// Read input file
+		List<String> input = readFile(INPUT_FILE);
+		
+		BinaryDiagnostic binaryDiagnostic = new BinaryDiagnostic();
+		
+		int result = binaryDiagnostic.solveLifeSupportRating(input);
+		assertEquals(7041258, result);
+	}
+	
+	private List<String> readFile(String inputFile) {
+		
+		Stream<String> lines = null;
+		List<String> input = null;
+		
+		try {
+			
+			Path path = Paths.get(getClass().getClassLoader().getResource(INPUT_FOLDER + inputFile).toURI());
+			input = Files.lines(path).collect(Collectors.toList());
+			
+		} catch (IOException e) {
+			input = new ArrayList<>();
+			e.printStackTrace();
+		} catch (URISyntaxException e) {
+			input = new ArrayList<>();
+			e.printStackTrace();
+		} finally {
+			if(lines != null) {
+				lines.close();
+			}
+		}
+		
+		return input;
+	}
+
+}
