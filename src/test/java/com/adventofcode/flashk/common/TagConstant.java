68160b858d8dd4cd5c2236ca302ed3cc7ef3b4ee
Flashk <mevengar@gmail.com>
2021-12-04 17:00:08 +0100

    Fix tag whitespaces

--- src/test/java/com/adventofcode/flashk/common/TagConstant.java
+++ src/test/java/com/adventofcode/flashk/common/TagConstant.java
@@ -6,7 +6,7 @@ public final class TagConstant {
 	
 	public final static String INPUT = "input";
 	public final static String SAMPLE = "sample";
-	public final static String PART_ONE = "part 1";
-	public final static String PART_TWO = "part 2";
+	public final static String PART_ONE = "part-1";
+	public final static String PART_TWO = "part-2";
 	
 }
