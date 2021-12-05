0d958d6a2a63ec3c428a57607ffe339e6f38e4f2
Flashk <mevengar@gmail.com>
2021-12-05 19:05:43 +0100

    Fix minor codacy issue

--- src/main/java/com/adventofcode/flashk/day5/Position.java
+++ src/main/java/com/adventofcode/flashk/day5/Position.java
@@ -26,8 +26,6 @@ public class Position {
 		Position other = (Position) obj;
 		if (x != other.x)
 			return false;
-		if (y != other.y)
-			return false;
-		return true;
+		return (y != other.y);
 	}
 }
