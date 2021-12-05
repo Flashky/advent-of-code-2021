8e0e8a3c1b8272349e3b9eee3272abf6335f491b
Flashk <mevengar@gmail.com>
2021-12-05 19:12:41 +0100

    Remove unused imports

--- src/test/java/com/adventofcode/flashk/common/Util.java
+++ src/test/java/com/adventofcode/flashk/common/Util.java
@@ -1,7 +1,6 @@
 package com.adventofcode.flashk.common;
 
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
