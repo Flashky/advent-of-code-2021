e9a6ccf6498768f3e3123527f199355cd3d7d31d
Flashk <mevengar@gmail.com>
2021-12-05 16:16:21 +0100

    Increase coverage

---
+++ src/test/java/com/adventofcode/flashk/day4/BingoLastWinStrategyEdgeCasesTest.java
@@ -0,0 +1,21 @@
+package com.adventofcode.flashk.day4;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class BingoLastWinStrategyEdgeCasesTest {
+	
+	@Test
+	void testEmptyNumberList() {
+		
+		BingoLastWinStrategy strategy = new BingoLastWinStrategy();
+		
+		int result = strategy.solve(new ArrayList<>(), new ArrayList<>(new ArrayList<>()));
+		
+		assertEquals(0, result);
+	}
+
+}
