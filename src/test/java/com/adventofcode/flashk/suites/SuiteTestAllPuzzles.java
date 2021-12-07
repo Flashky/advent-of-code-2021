131c1c31f3d86322f61316ec470bf52e62391035
Flashk <mevengar@gmail.com>
2021-12-07 21:31:00 +0100

    Add new test suite to test all puzzles

---
+++ src/test/java/com/adventofcode/flashk/suites/SuiteTestAllPuzzles.java
@@ -0,0 +1,16 @@
+package com.adventofcode.flashk.suites;
+
+import org.junit.platform.suite.api.IncludeTags;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+import org.junit.platform.suite.api.SuiteDisplayName;
+
+import com.adventofcode.flashk.common.TagConstant;
+
+@Suite
+@IncludeTags({ TagConstant.PART_ONE, TagConstant.PART_TWO })
+@SelectPackages({"com.adventofcode.flashk"})
+@SuiteDisplayName("Test all puzzles")
+public class SuiteTestAllPuzzles {
+
+}
