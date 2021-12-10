95462284abed2219564082d697c417c8e7b57a45
Flashky <mevengar@gmail.com>
2021-12-10 07:36:47 +0100

    Solve day 10 puzzle

---
+++ src/test/java/com/adventofcode/flashk/day10/Day10Test.java
@@ -0,0 +1,132 @@
+package com.adventofcode.flashk.day10;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import com.adventofcode.flashk.common.test.utils.DisplayNameConstant;
+import com.adventofcode.flashk.common.test.utils.PuzzleTest;
+import com.adventofcode.flashk.common.test.utils.TagConstant;
+import com.adventofcode.flashk.common.test.utils.Util;
+
+@DisplayName("Day 10")
+@TestMethodOrder(OrderAnnotation.class)
+public class Day10Test extends PuzzleTest {
+
+	private final static String INPUT_FOLDER = "day10";
+	private final static String INPUT_FILE = "data.input";
+	private final static String INPUT_FILE_SAMPLE = "sample.input";
+
+	@BeforeAll
+	public static void beforeAll() {
+		System.out.println();
+		System.out.println("========= Day 10 ==========");
+		System.out.println("---------------------------");
+		System.out.println("# | Input  | Elapsed time");
+		System.out.println("---------------------------");
+	}
+
+	
+	@Test
+	@Order(1)
+	@Tag(TagConstant.PART_ONE)
+	@Tag(TagConstant.SAMPLE)
+	@DisplayName(DisplayNameConstant.PART_ONE_SAMPLE)
+	public void testSolvePart1Sample() {
+		
+		System.out.print("1 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE_SAMPLE);
+		
+		SyntaxScoring syntaxScoring = new SyntaxScoring(inputs);
+		int result = syntaxScoring.solve();
+		
+		assertEquals(26397, result);
+	}
+	
+	@Test
+	@Order(2)
+	@Tag(TagConstant.PART_ONE)
+	@Tag(TagConstant.INPUT)
+	@DisplayName(DisplayNameConstant.PART_ONE_INPUT)
+	public void testSolvePart1Input() {
+		
+		System.out.print("1 | input  | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE);
+		
+		SyntaxScoring syntaxScoring = new SyntaxScoring(inputs);
+		int result = syntaxScoring.solve();
+		
+		assertEquals(394647, result);
+		
+	}
+	
+	@Test
+	@Order(3)
+	@Tag(TagConstant.PART_TWO)
+	@Tag(TagConstant.SAMPLE)
+	@DisplayName(DisplayNameConstant.PART_TWO_SAMPLE)
+	public void testSolvePart2Sample() {
+		
+		System.out.print("2 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE_SAMPLE);
+		
+		SyntaxScoring syntaxScoring = new SyntaxScoring(inputs);
+		long result = syntaxScoring.solveB();
+		
+		assertEquals(288957L, result);
+		
+	}
+	
+	@Test
+	@Order(3)
+	@Tag(TagConstant.PART_TWO)
+	@Tag(TagConstant.DEBUG)
+	@DisplayName(DisplayNameConstant.PART_TWO_SAMPLE)
+	public void testSolvePart2SingleSample294() {
+		
+		System.out.print("2 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, "debug_294.input");
+		
+		SyntaxScoring syntaxScoring = new SyntaxScoring(inputs);
+		long result = syntaxScoring.solveB();
+		
+		assertEquals(294L, result);
+		
+	}
+	
+	@Test
+	@Order(4)
+	@Tag(TagConstant.PART_TWO)
+	@Tag(TagConstant.INPUT)
+	@DisplayName(DisplayNameConstant.PART_TWO_INPUT)
+	public void testSolvePart2Input() {
+		
+		System.out.print("2 | input  | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE);
+		
+		SyntaxScoring syntaxScoring = new SyntaxScoring(inputs);
+		long result = syntaxScoring.solveB();
+		
+		assertEquals(2380061249L, result);
+		
+	}
+
+}
