2caca9ef53ad2d1cded696e5298012cb7c947546
Flashk <mevengar@gmail.com>
2021-12-12 10:54:57 +0100

    Solve day 12 puzzle

---
+++ src/main/java/com/adventofcode/flashk/day12/PassagePathing.java
@@ -0,0 +1,230 @@
+package com.adventofcode.flashk.day12;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PassagePathing {
+
+	private final static String INPUT_PATTERN = "(start|end|[\\w]*)-(start|end|[\\w]*)";
+	private final static Pattern EDGE_PATTERN = Pattern.compile(INPUT_PATTERN);
+	private final static String START_CAVE = "start";
+	private final static String END_CAVE = "end";
+	
+	//private List<List<String>> paths;
+	private Map<String,List<String>> caveAdjacency = new HashMap<>();
+	private Map<String,Integer> visitedSmallCaveCount = new HashMap<>();
+	
+	private int maxSmallCaveVisits;
+	private boolean visitedSmallCaveMax;
+	
+	
+	public PassagePathing(List<String> inputs) {
+	
+		for(String input : inputs) {
+			Matcher matcher = EDGE_PATTERN.matcher(input);
+			matcher.find();
+			
+			String origin = matcher.group(1);
+			String destination = matcher.group(2);
+			
+			if(START_CAVE.equals(origin)) {
+				
+				// Start nodes can only be origin nodes: flip from destination to origin
+				List<String> edges = getOrCreate(START_CAVE);
+				edges.add(destination);
+				
+				if(isSmallCave(destination)) {
+					visitedSmallCaveCount.put(destination, 0);
+				}
+				
+			} else if(START_CAVE.equals(destination)) {
+				
+				// Start nodes can only be origin nodes: flip from destination to origin
+				
+				List<String> edges = getOrCreate(START_CAVE);
+				edges.add(origin);
+				
+				if(isSmallCave(origin)) {
+					visitedSmallCaveCount.put(origin, 0);
+				}
+				
+			} else if(END_CAVE.equals(origin)) {
+				
+				// End nodes can only be destination nodes: flip from origin to destination
+				List<String> edges = getOrCreate(destination);
+				edges.add(END_CAVE);
+				
+				if(isSmallCave(destination)) {
+					visitedSmallCaveCount.put(destination, 0);
+				}
+				
+			} else if (END_CAVE.equals(destination)) {
+				
+				// End nodes can only be destination nodes: flip from origin to destination
+				List<String> edges = getOrCreate(origin);
+				edges.add(END_CAVE);
+				
+				if(isSmallCave(origin)) {
+					visitedSmallCaveCount.put(origin, 0);
+				}
+				
+				
+			} else {
+				
+				// Any cave to any other cave different than 'start' or 'end':
+				// Add bidirectional connection
+				
+				List<String> edges = getOrCreate(origin);
+				edges.add(destination);
+				
+				if(isSmallCave(destination)) {
+					visitedSmallCaveCount.put(destination, 0);
+				}
+				
+				edges = getOrCreate(destination);
+				edges.add(origin);
+				
+				if(isSmallCave(origin)) {
+					visitedSmallCaveCount.put(origin, 0);
+				}
+				
+			}
+			
+		}
+		
+	}
+	
+	public int solveA() {
+		
+		int result = 0;
+		
+		// Explore starting from start node
+		List<String> adjacentCaves = caveAdjacency.get(START_CAVE);
+		//paths = new ArrayList<>();
+		
+		for(String cave : adjacentCaves) {
+			
+			List<String> currentPath = new ArrayList<>();
+			currentPath.add(START_CAVE);
+			
+			result += findPaths(cave, currentPath);
+		}
+		
+		return result;
+	}
+	
+	private int findPaths(String currentCave, List<String> currentPath) {
+		
+		if(END_CAVE.equals(currentCave)) {
+			return 1;
+		} else if(isSmallCave(currentCave) && visitedSmallCaveCount.get(currentCave) == 1) {
+			return 0; // Invalid path - Already visited small cave
+		} else {
+		
+			int paths = 0;
+		
+			currentPath.add(currentCave);
+			
+			if(isSmallCave(currentCave)) {
+				Integer visitedTimes = visitedSmallCaveCount.get(currentCave);
+				visitedSmallCaveCount.put(currentCave, ++visitedTimes);
+			}
+			
+			List<String> adjacentCaves = caveAdjacency.get(currentCave);
+			
+			for(String cave : adjacentCaves) {
+				paths += findPaths(cave, currentPath);
+			}
+			
+			if(isSmallCave(currentCave)) {
+				Integer visitedTimes = visitedSmallCaveCount.get(currentCave);
+				visitedSmallCaveCount.put(currentCave, --visitedTimes);
+			}
+			return paths;
+		}
+		
+	}
+	
+	private int findPathsB(String currentCave, List<String> currentPath) {
+		
+		if(END_CAVE.equals(currentCave)) {
+			return 1;
+		} else if(isSmallCave(currentCave) && visitedSmallCaveCount.get(currentCave) >= 1 && visitedSmallCaveMax) {
+			return 0; // Invalid path - Already visited small cave twice at least once
+		} else {
+		
+			int paths = 0;
+		
+			currentPath.add(currentCave);
+			
+			if(isSmallCave(currentCave)) {
+				Integer visitedTimes = visitedSmallCaveCount.get(currentCave);
+				visitedTimes++;
+				visitedSmallCaveCount.put(currentCave, visitedTimes);
+				
+				if(visitedTimes == 2) {
+					visitedSmallCaveMax = true;
+				}
+			}
+			
+			List<String> adjacentCaves = caveAdjacency.get(currentCave);
+			
+			for(String cave : adjacentCaves) {
+				paths += findPathsB(cave, currentPath);
+			}
+			
+			if(isSmallCave(currentCave)) {
+				Integer visitedTimes = visitedSmallCaveCount.get(currentCave);
+				visitedTimes--;
+				visitedSmallCaveCount.put(currentCave, visitedTimes);
+				
+				if(visitedTimes == 1) {
+					visitedSmallCaveMax = false;
+				}
+			}
+			return paths;
+		}
+		
+	}
+	
+	public int solveB() {
+		
+		int result = 0;
+		
+		// Explore starting from start node
+		List<String> adjacentCaves = caveAdjacency.get(START_CAVE);
+		visitedSmallCaveMax = false;
+		
+		for(String cave : adjacentCaves) {
+			
+			List<String> currentPath = new ArrayList<>();
+			currentPath.add(START_CAVE);
+			
+			result += findPathsB(cave, currentPath);
+		}
+		
+		return result;
+	}
+	
+	private boolean isSmallCave(String cave) {
+		return cave.toLowerCase().equals(cave);
+	}
+	
+	private List<String> getOrCreate(String origin) {
+		
+		List<String> edges = caveAdjacency.get(origin);
+		
+		if(edges == null) {
+			edges = new ArrayList<>();
+			caveAdjacency.put(origin, edges);
+		}
+		
+		return edges;
+	}
+}
---
+++ src/test/java/com/adventofcode/flashk/day12/Day12Test.java
@@ -0,0 +1,171 @@
+package com.adventofcode.flashk.day12;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import com.adventofcode.flashk.common.test.constants.TestDisplayName;
+import com.adventofcode.flashk.common.test.constants.TestFilename;
+import com.adventofcode.flashk.common.test.constants.TestTag;
+import com.adventofcode.flashk.common.test.utils.PuzzleTest;
+import com.adventofcode.flashk.common.test.utils.Timer;
+import com.adventofcode.flashk.common.test.utils.Util;
+
+@DisplayName(TestDisplayName.DAY_12)
+@TestMethodOrder(OrderAnnotation.class)
+public class Day12Test extends PuzzleTest {
+
+	private final static String INPUT_FOLDER = "day_12";
+	private final static String INPUT_FILE_MEDIUM_SAMPLE = "sample_medium.input";
+	private final static String INPUT_FILE_LARGE_SAMPLE = "sample_large.input";
+	
+	@BeforeAll
+	public static void beforeAll() {
+		Timer.printHeader(TestDisplayName.DAY_12);
+	}
+
+	
+	@Test
+	@Order(1)
+	@Tag(TestTag.PART_ONE)
+	@Tag(TestTag.SAMPLE)
+	@DisplayName(TestDisplayName.PART_ONE_SAMPLE)
+	public void testSolvePart1Sample() {
+		
+		System.out.print("1 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE_SAMPLE);
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		
+		assertEquals(10, passagePathing.solveA());
+	}
+	
+	@Test
+	@Order(2)
+	@Tag(TestTag.PART_ONE)
+	@Tag(TestTag.SAMPLE)
+	@DisplayName(TestDisplayName.PART_ONE_SAMPLE)
+	public void testSolvePart1SampleMedium() {
+		
+		System.out.print("1 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE_MEDIUM_SAMPLE);
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		
+		assertEquals(19, passagePathing.solveA());
+		
+	}
+	
+	@Test
+	@Order(3)
+	@Tag(TestTag.PART_ONE)
+	@Tag(TestTag.SAMPLE)
+	@DisplayName(TestDisplayName.PART_ONE_SAMPLE)
+	public void testSolvePart1SampleLarge() {
+		
+		System.out.print("1 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE_LARGE_SAMPLE);
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		
+		assertEquals(226, passagePathing.solveA());
+		
+	}
+	
+	@Test
+	@Order(4)
+	@Tag(TestTag.PART_ONE)
+	@Tag(TestTag.INPUT)
+	@DisplayName(TestDisplayName.PART_ONE_INPUT)
+	public void testSolvePart1Input() {
+		
+		System.out.print("1 | input  | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE);
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		
+		assertEquals(3761, passagePathing.solveA());
+	}
+	
+	@Test
+	@Order(5)
+	@Tag(TestTag.PART_TWO)
+	@Tag(TestTag.SAMPLE)
+	@DisplayName(TestDisplayName.PART_TWO_SAMPLE)
+	public void testSolvePart2Sample() {
+		
+		System.out.print("2 | sample | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE_SAMPLE);
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		
+		assertEquals(36, passagePathing.solveB());
+
+	}
+	
+	@Test
+	@Order(6)
+	@Tag(TestTag.PART_TWO)
+	@Tag(TestTag.SAMPLE)
+	@DisplayName(TestDisplayName.PART_TWO_SAMPLE)
+	public void testSolvePart2SampleMedium() {
+		
+		System.out.print("2 | sample medium | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE_MEDIUM_SAMPLE);
+		
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		assertEquals(103, passagePathing.solveB());
+
+	}
+	
+	
+	@Test
+	@Order(7)
+	@Tag(TestTag.PART_TWO)
+	@Tag(TestTag.SAMPLE)
+	@DisplayName(TestDisplayName.PART_TWO_SAMPLE)
+	public void testSolvePart2SampleLarge() {
+		
+		System.out.print("2 | sample large | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, INPUT_FILE_LARGE_SAMPLE);
+		
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		assertEquals(3509, passagePathing.solveB());
+
+	}
+	
+	@Test
+	@Order(8)
+	@Tag(TestTag.PART_TWO)
+	@Tag(TestTag.INPUT)
+	@DisplayName(TestDisplayName.PART_TWO_INPUT)
+	public void testSolvePart2Input() {
+		
+		System.out.print("2 | input  | ");
+		
+		// Read input file
+		List<String> inputs = Util.readStringLines(INPUT_FOLDER, TestFilename.INPUT_FILE);
+		
+		PassagePathing passagePathing = new PassagePathing(inputs);
+		assertEquals(99138, passagePathing.solveB());
+		
+	}
+
+}
