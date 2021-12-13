d8c8ed9e90f044a4f12cebbf5ac1aad36b0460ae
Flashk <mevengar@gmail.com>
2021-12-13 17:59:43 +0100

    Add non-static version of substractAbs to Vector2

--- src/main/java/com/adventofcode/flashk/common/Vector2.java
+++ src/main/java/com/adventofcode/flashk/common/Vector2.java
@@ -24,6 +24,31 @@ public class Vector2 {
 		this.y += direction.getY();
 	}
 	
+	
+	/**
+	 * Substracts the right operand vector to the left operand vector, applying absolute value to the result.
+	 *  
+	 * <p>
+	 * Examples <code>(x,y)</code>:
+	 * </p>
+	 * <pre>
+	 * |(0,14) - (0,7)| = |(0,7)| 	= <b>(0,7)</b>
+	 * |(0,7) - (0,14)| = |(0,-7)| 	= <b>(0,7)</b>
+	 * |(11,0) - (5,0)| = |(6,0)| 	= <b>(6,0)</b>
+	 * |(5,0) - (11,0)| = |(-6,0)| 	= <b>(6,0)</b>
+	 * </code>
+	 *  
+	 * </p>
+	 * @param other substracting Vector2.
+	 * @return a new Vector2
+	 */
+	public void substractAbs(Vector2 other) {
+		
+		this.x = Math.abs(this.x - other.x);
+		this.y = Math.abs(this.y - other.y);
+		
+	}
+	
 	public void normalize() {
 		
 		double length = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
