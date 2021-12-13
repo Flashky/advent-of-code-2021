84346806d7c3f3dc8257335d56b6b224e11abc40
Flashky <mevengar@gmail.com>
2021-12-13 13:11:31 +0100

    Solve day 13 puzzle

---
+++ src/main/java/com/adventofcode/flashk/day13/Instruction.java
@@ -0,0 +1,13 @@
+package com.adventofcode.flashk.day13;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Instruction {
+
+	private String axis;
+	private Integer value;
+	
+}
