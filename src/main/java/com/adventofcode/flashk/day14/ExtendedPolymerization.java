549efb181c60d81bfaa3a7847e3e66a3ff12409b
Flashk <mevengar@gmail.com>
2021-12-14 21:22:12 +0100

    Solve day 14 puzzle part 2

--- src/main/java/com/adventofcode/flashk/day14/ExtendedPolymerization.java
+++ src/main/java/com/adventofcode/flashk/day14/ExtendedPolymerization.java
@@ -1,27 +1,23 @@
 package com.adventofcode.flashk.day14;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class ExtendedPolymerization {
 
 	private final static Pattern INSERTION_RULE_PATTERN = Pattern.compile("([A-Z][A-Z]) -> ([A-Z])");
-	
-	private String polymer = "";
-	private Map<String, Character> insertionRules = new HashMap<>();
-	private Deque<Character> polymerizator = new ArrayDeque<>(); 
+
+	private Map<String, Character> formulas = new HashMap<>();
 	private Map<Character, Long> elementCount = new HashMap<>();
-	
-	private Long mostCommonCount = Long.MIN_VALUE;
-	private Long lessCommonCount = Long.MAX_VALUE;
+	private Map<String, Long> pairsCount = new HashMap<>();
 	
 	public ExtendedPolymerization(List<String> inputs) {
 	
+		String polymer;
 		for(String input : inputs) {
 			
 			Matcher matcher = INSERTION_RULE_PATTERN.matcher(input);
@@ -29,40 +25,55 @@ public class ExtendedPolymerization {
 			if(matcher.find()) {
 				String adjacentElements = matcher.group(1);
 				Character elementToInsert = matcher.group(2).charAt(0);
-				insertionRules.put(adjacentElements, elementToInsert);
+				formulas.put(adjacentElements, elementToInsert);
+				
 			} else if(!"".equals(input)){
 				polymer = input;
 				
+				// Count initial pair occurrences
+				String polymerSplitter = polymer;
+				while(polymerSplitter.length() > 1) {
+					String pair = polymerSplitter.substring(0, 2);
+					polymerSplitter = polymerSplitter.substring(1, polymerSplitter.length());
+					
+					Long pairCount = pairsCount.getOrDefault(pair, 0L) + 1L;
+					pairsCount.put(pair, pairCount);
+				}
+				
+				// Count initial character occurrences
 				char[] polymerElements = polymer.toCharArray();
 				for(char element : polymerElements) {
-					polymerizator.add(element);
 					updateElementCount(element);
 				}
 			}
 		}
 	}
 	
-	
 	public Long solveA(int times) {
 
 		for(int i = 0; i < times; i++) {
-			Deque<Character> polymerizatedResult = new ArrayDeque<>();
-			while(polymerizator.size() > 1) {
-				
-				Character first = polymerizator.poll();
-				Character second = polymerizator.peek();
-				String polymer = String.valueOf(first) + String.valueOf(second);
-				Character elementToInsert = insertionRules.get(polymer);
-				
-				polymerizatedResult.add(first);
-				polymerizatedResult.add(elementToInsert);
-				
-				updateElementCount(elementToInsert);
-			}
 			
-			// Last missing character
-			polymerizatedResult.add(polymerizator.poll());
-			polymerizator = polymerizatedResult;
+			Map<String, Long> newPairsCount = new HashMap<>();
+			for(Entry<String, Long> entryPair : pairsCount.entrySet()) {
+				
+				// Get data
+				String pair = entryPair.getKey();
+				Long occurrences = entryPair.getValue();
+				
+				// Update element count
+				Character element = formulas.get(pair);
+				updateElementCount(element, occurrences);
+				
+				// Create two new pairs and add it to the next iteration map
+				String newPair = pair.substring(0,1) + element;
+				Long pairCount = newPairsCount.getOrDefault(newPair, 0L) + occurrences;
+				newPairsCount.put(newPair, pairCount);
+				
+				newPair = element + pair.substring(1,2);
+				pairCount = newPairsCount.getOrDefault(newPair, 0L) + occurrences;
+				newPairsCount.put(newPair, pairCount);
+			}
+			pairsCount = newPairsCount;
 		}
 		
 		Long max = elementCount.values().stream().mapToLong(v -> v).max().orElse(Long.MIN_VALUE);
@@ -71,12 +82,12 @@ public class ExtendedPolymerization {
 		return max-min;
 	}
 	
+	private void updateElementCount(Character element, Long times) {
+		Long elementCount = times + this.elementCount.getOrDefault(element, 0L);
+		this.elementCount.put(element, elementCount);
+	}
+	
 	private void updateElementCount(Character element) {
-		if(!elementCount.containsKey(element)) {
-			elementCount.put(element, 1L);
-		} else {
-			Long count = elementCount.get(element) + 1;
-			elementCount.put(element, count);
-		}
+		updateElementCount(element,1L);
 	}
 }
