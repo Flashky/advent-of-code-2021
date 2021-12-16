071f592de38492d54873a68ac584618d45c70571
Flashky <mevengar@gmail.com>
2021-12-16 17:03:39 +0100

    Day 16 WIP

--- src/main/java/com/adventofcode/flashk/day16/PacketNode.java
+++ src/main/java/com/adventofcode/flashk/day16/PacketNode.java
@@ -2,4 +2,9 @@ package com.adventofcode.flashk.day16;
 
 public class PacketNode {
 
+	private Integer version;
+	private Integer typeId;
+	private LengthTypeId lengthTypeId;
+	private Integer headerSize;
+	private boolean literalPacket;
 }
