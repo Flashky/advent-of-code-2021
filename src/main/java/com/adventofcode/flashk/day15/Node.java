adf71d15c1dfb4ad3caae85b0b83044a5caeee31
Flashk <mevengar@gmail.com>
2021-12-18 16:06:06 +0100

    Minor refactors to day 15

--- src/main/java/com/adventofcode/flashk/day15/Node.java
+++ src/main/java/com/adventofcode/flashk/day15/Node.java
@@ -2,36 +2,31 @@ package com.adventofcode.flashk.day15;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
+@Setter
 public class Node implements Comparable<Node> {
 
 	private final Integer x;
 	private final Integer y;
-
-	@Setter
+	
 	private Integer totalRisk = Integer.MAX_VALUE;
-	
-	@Setter
 	private Integer risk;
-	
-	@Setter
 	private Node parent = null;
-	
-	@Setter
 	private boolean visited = false;
-	
-	public Node(Integer x, Integer y) {
+
+	public Node(Integer x, Integer y, Integer risk) {
 		this.x = x;
 		this.y = y;
+		this.risk = risk;
 	}
-
+	
 	@Override
 	public int compareTo(Node other) {
 		return Integer.compare(totalRisk, other.totalRisk);
 	}
-
+	
+	/*
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -39,39 +34,6 @@ public class Node implements Comparable<Node> {
 		result = prime * result + ((x == null) ? 0 : x.hashCode());
 		result = prime * result + ((y == null) ? 0 : y.hashCode());
 		return result;
-	}
+	}*/
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Node other = (Node) obj;
-		if (x == null) {
-			if (other.x != null)
-				return false;
-		} else if (!x.equals(other.x))
-			return false;
-		if (y == null) {
-			if (other.y != null)
-				return false;
-		} else if (!y.equals(other.y))
-			return false;
-		return true;
-	}
-
-	@Override
-	public String toString() {
-		return "Node [x=" + x + ", y=" + y + ", totalRisk=" + totalRisk + ", risk=" + risk + ", parent=" + parent
-				+ ", visited=" + visited + "]";
-	}
-	
-	
-
-
-
-	    
 }
