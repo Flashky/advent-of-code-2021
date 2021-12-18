f6d46a65a8abae70847ccd4b315da1031a4c7bad
Flashk <mevengar@gmail.com>
2021-12-18 18:09:01 +0100

    Fix minor issues at Vector2

--- src/main/java/com/adventofcode/flashk/common/Vector2.java
+++ src/main/java/com/adventofcode/flashk/common/Vector2.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
-//@EqualsAndHashCode
+@EqualsAndHashCode
 @ToString
 public class Vector2 {
 
@@ -90,31 +90,6 @@ public class Vector2 {
 		
 		return new Vector2(x,y);
 		
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + x;
-		result = prime * result + y;
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Vector2 other = (Vector2) obj;
-		if (x != other.x)
-			return false;
-		if (y != other.y)
-			return false;
-		return true;
 	}
 	
 	
