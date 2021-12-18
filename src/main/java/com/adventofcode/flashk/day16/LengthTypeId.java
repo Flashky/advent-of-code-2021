2043be4101f396ee786112b7a60a9b450672e8a2
Flashk <mevengar@gmail.com>
2021-12-18 20:45:37 +0100

    Solve Day 16 part 1 puzzle

--- src/main/java/com/adventofcode/flashk/day16/LengthTypeId.java
+++ src/main/java/com/adventofcode/flashk/day16/LengthTypeId.java
@@ -1,25 +1,23 @@
 package com.adventofcode.flashk.day16;
 
-import lombok.Getter;
+import com.adventofcode.flashk.common.BaseUtil;
 
 public enum LengthTypeId {
 
-	SUBPACKETS_LENGTH("0"),
-	SUBPACKETS_NUMBER("1");
+	SUBPACKETS_LENGTH,
+	SUBPACKETS_NUMBER;
 	
-	@Getter
-	private String value;
-	
-	private LengthTypeId(String value) {
-		this.value = value;
-	}
-	
-	public static LengthTypeId fromBinValue(String value) {
-		switch(value) {
-			case "0": return SUBPACKETS_LENGTH;
-			case "1": return SUBPACKETS_NUMBER;
+	public static LengthTypeId fromId(Integer id) {
+		
+		switch(id) {
+			case 0: return SUBPACKETS_LENGTH;
+			case 1: return SUBPACKETS_NUMBER;
 			default: return null;
 		}
+	}
+	
+	public static LengthTypeId fromId(String binaryId) {
+		return LengthTypeId.fromId(BaseUtil.binaryToDecInteger(binaryId));
 	}
 	
 }
---
+++ src/main/java/com/adventofcode/flashk/day16/Packet.java
@@ -0,0 +1,25 @@
+package com.adventofcode.flashk.day16;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Packet {
+
+	private Integer version;
+	private Integer typeId;
+	private LengthTypeId lengthTypeId;
+	
+	// Only operators
+	private Integer subpacketsLength;
+	private Integer subpacketsNumber;
+	
+	// Only literals
+	private Long literal;
+	
+	private Integer length;
+	
+}
