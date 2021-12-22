325bb9a816111bb0cb9df2fc268033426ab721ef
Flashk <mevengar@gmail.com>
2021-12-22 20:23:28 +0100

    Prepare day 18

--- src/test/java/com/adventofcode/flashk/common/test/constants/TestDisplayName.java
+++ src/test/java/com/adventofcode/flashk/common/test/constants/TestDisplayName.java
@@ -23,6 +23,7 @@ public final class TestDisplayName {
 	public final static String DAY_15 = "Day 15";
 	public final static String DAY_16 = "Day 16";
 	public final static String DAY_17 = "Day 17";
+	public final static String DAY_18 = "Day 18";
 	public final static String DAY_21 = "Day 21";
 	
 	// Single test display name
