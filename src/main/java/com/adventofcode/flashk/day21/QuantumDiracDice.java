69a77fea2b5ee795a561f4c9efb858ca17d01495
Flashky <mevengar@gmail.com>
2021-12-22 11:28:09 +0100

    Solve day 21 part 2 puzzle

--- src/main/java/com/adventofcode/flashk/day21/QuantumDiracDice.java
+++ src/main/java/com/adventofcode/flashk/day21/QuantumDiracDice.java
@@ -1,98 +1,103 @@
 package com.adventofcode.flashk.day21;
 
-import java.util.ArrayDeque;
-import java.util.Deque;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
-import lombok.Getter;
 
 public class QuantumDiracDice {
 
 	private static final Pattern PLAYER_POSITIONS_PATTERN = Pattern.compile("position: ([0-9]*)");
 	
 	private static final int MAX_SCORE = 21;
-	private static final int MAX_DICE_VALUE = 3;
-	private static final int MIN_DICE_VALUE = 1;
-	private static final int ROLLS_PER_TURN = 3;
 
 	private int initialP1Position;
 	private int initialP2Position;
+	
+	// Any single roll has 3 possible values: {1,2,3}
+	// There are 3 rolls per turn = 3^3 = 27 total roll value combinations
+	// Some roll values yield the same result. Frequency analysis:
+	// - A total roll of 3 happens 1 times.
+	// - A total roll of 4 happens 3 times.
+	// - A total roll of 5 happens 6 times.
+	// - A total roll of 6 happens 7 times.
+	// - A total roll of 8 happens 3 times.
+	// - A total roll of 9 happens 1 times.
+	
+	// This map contains the relationship between total roll value and frequency:
+	private Map<Integer, Integer> rollFrequency = new HashMap<>();
 	
 	public QuantumDiracDice(List<String> inputs) {
 		
 		Matcher playerMatcher = PLAYER_POSITIONS_PATTERN.matcher(inputs.get(0));
 		
 		playerMatcher.find();
-		//Player player = new Player(Integer.valueOf(playerMatcher.group(1)));
 		initialP1Position = Integer.valueOf(playerMatcher.group(1));
 		
 		playerMatcher = PLAYER_POSITIONS_PATTERN.matcher(inputs.get(1));
 		
 		playerMatcher.find();
-		//player = new Player(Integer.valueOf(playerMatcher.group(1)));
 		initialP2Position = Integer.valueOf(playerMatcher.group(1));
+		
+		// Frequency map
+		rollFrequency.put(3, 1);
+		rollFrequency.put(4, 3);
+		rollFrequency.put(5, 6);
+		rollFrequency.put(6, 7);
+		rollFrequency.put(7, 6);
+		rollFrequency.put(8, 3);
+		rollFrequency.put(9, 1);
+		
 	}
 
 	
 	public long solveB() {
-		
-		long[] scoreboard = new long[2];
-		scoreboard[0] = 0L;
-		scoreboard[1] = 0L;
-	
-		roll(initialP1Position, 0, initialP2Position, 0, 0, 3, scoreboard);
-		
-		if(scoreboard[0] > scoreboard[1]) {
-			return scoreboard[0];
-		} 
-		
-		return scoreboard[1];
-	}
-	
-	private void roll(int p1Position, int p1Score, 
-						int p2Position, int p2Score, 
-						int turn, int pendingRolls,
-						long[] scoreboard) {
 
-		if(p1Score >= MAX_SCORE) {
-			scoreboard[0]++;
-		} else if(p2Score >= MAX_SCORE) {
-			scoreboard[1]++; 
-		} else if(pendingRolls == 0) {
-			
-			if(turn == 0) {
-				int nextP1Score = p1Score + p1Position;
-				roll(p1Position, nextP1Score, p2Position, p2Score, 1, 3, scoreboard);
-			} else {
-				int nextP2Score = p2Score + p2Position;
-				roll(p1Position, p1Score, p2Position, nextP2Score, 0, 3, scoreboard);
-			}
-			
-		} else {
-			
-			// One rolled dice, different universe posibilities
-			for(int diceValue = MIN_DICE_VALUE; diceValue <= MAX_DICE_VALUE; diceValue++) {
-				
-				if(turn == 0) {
-
-					// Update player position, score and turn if applies
-					int nextP1Position = move(diceValue, p1Position);
-					roll(nextP1Position, p1Score, p2Position, p2Score, turn, pendingRolls-1, scoreboard);
-					
-				} else {
-					
-					// Update player position, score and turn if applies
-					int nextP2Position = move(diceValue, p2Position);
-					roll(p1Position, p1Score, nextP2Position, p2Score, turn, pendingRolls-1, scoreboard);
-				}
-				
-			}
+		Scoreboard totalScoreboard = rollPartial(initialP1Position, 0, initialP2Position, 0, 0);
+		
+		if(totalScoreboard.getP1Score() > totalScoreboard.getP2Score()) {
+			return totalScoreboard.getP1Score();
 		}
 		
+		return totalScoreboard.getP2Score();
 	}
 
+	private Scoreboard rollPartial(int p1Position, int p1Score, int p2Position, int p2Score, int turn) {
+
+		if(p1Score >= MAX_SCORE) {
+			return new Scoreboard(1,0);
+		} else if(p2Score >= MAX_SCORE) {
+			return new Scoreboard(0,1);
+		} else {
+			
+			Scoreboard totalScoreboard = new Scoreboard(0,0);
+			
+			// Iterates through all possible total roll results
+			for(int totalRollValue = 3; totalRollValue <= 9; totalRollValue++) {
+			
+				Scoreboard childScoreboard;
+				if(turn == 0) {
+					int nextP1Position = move(totalRollValue, p1Position);
+					int nextP1Score = p1Score + nextP1Position;
+					childScoreboard = rollPartial(nextP1Position, nextP1Score, p2Position, p2Score, 1);
+				} else {
+					int nextP2Position = move(totalRollValue, p2Position);
+					int nextP2Score = p2Score + nextP2Position;
+					childScoreboard = rollPartial(p1Position, p1Score, nextP2Position, nextP2Score, 0);
+				}
+				
+				// Multiply the scoreboard by its frequency and add to the total scoreboard
+				childScoreboard.multiply(rollFrequency.get(totalRollValue));
+				totalScoreboard.sum(childScoreboard);
+			}
+			
+			return totalScoreboard;
+			
+		}
+			
+		
+	}
 	
 	private int move(int rollValue, int currentPosition) {
 
