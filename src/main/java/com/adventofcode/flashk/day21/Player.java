39ed296141b25ae6df39d0ad49fc05582f4b0809
Flashky <mevengar@gmail.com>
2021-12-22 12:15:52 +0100

    Clean up day 21 code

--- src/main/java/com/adventofcode/flashk/day21/Player.java
+++ src/main/java/com/adventofcode/flashk/day21/Player.java
@@ -7,43 +7,28 @@ public class Player {
 	
 	private int score = 0;
 	private int position;
-	private int pendingRolls = 3;
 	
 	public Player(int position) {
 		this.position = position;
 	}
 	
-	public void move(int rollValue) {
+	public void move(int totalRollValue) {
 
-		if(hasPendingRolls() ) {
-			pendingRolls--;
-			
-			int estimatedPosition = position + rollValue;
-			if(estimatedPosition < 10) {
-				position = estimatedPosition;
+		int finalPosition = position + totalRollValue;
+		
+		if(finalPosition < 10) {
+			position = finalPosition;
+		} else {
+			finalPosition = finalPosition % 10;
+			if(finalPosition == 0) {
+				position = 10;
 			} else {
-				
-				estimatedPosition = estimatedPosition % 10;
-				if(estimatedPosition == 0) {
-					position = 10;
-				} else {
-					position = estimatedPosition;
-				}
-			}
-			
-			if(pendingRolls == 0) {
-				score += position;
+				position = finalPosition;
 			}
 		}
 
-	}
-	
-	public void resetPendingRolls() {
-		pendingRolls = 3;
-	}
-	
-	public boolean hasPendingRolls() {
-		return pendingRolls > 0;
+		score += position;
+
 	}
 
 }
